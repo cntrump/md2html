393f17940831f89ae352e05a5f0cf99906a21fb4
vvveiii <cntrump@gmail.com>
2021-01-26 21:48:14 +0800

    sync main branch

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM pandoc/core AS base
+
+RUN apk update && \
+    apk add zsh && \
+    rm -rf /var/cache/apk/*
+
+ADD syntax.theme /usr/local/md2html/
+ADD template.html /usr/local/md2html/
+ADD style.css /usr/local/md2html/
+ADD md2html.sh /usr/local/md2html/
+
+WORKDIR /data
+
+ENTRYPOINT ["/usr/local/md2html/md2html.sh"]
--- md2html.sh
+++ md2html.sh
@@ -2,40 +2,82 @@
 
 set -e
 
-APP_DIR=${0:a:h}
-pushd "${APP_DIR}"
-
-ARGS=$#
-if [ ${ARGS} -eq 0 ];then
-  echo useage: "md2html file [toc_depth]"
-  echo example: md2html ./markdown.md
-  echo example: md2html ./markdown.md 2
+print_usage_and_exit() {
+  echo "useage:"
+  echo "\tmd2html [-t 2] [-l zh] [-s] -i /path/to/markdown.md"
+  echo "options:"
+  echo "\t-t N: toc-depth, N must >= 1, default: 2."
+  echo "\t-l en: lang, default: en."
+  echo "\t-s: create self contained html, inline all resources."
+  echo "\t-h: display this infomation."
+  echo "example:"
+  echo "\tmd2html -i ./markdown.md"
+  echo "\tmd2html -i ./markdown.md -t 2"
+  echo "\tmd2html -i ./markdown.md -t 2 -l zh"
+  echo "\tmd2html -i ./markdown.md -s"
   exit -1
-fi
+}
 
-INPUT=$1
-OUTPUT=${1:a:r}.html
-
+LANG="en"
 TOC_DEPTH=2
-if [ ${ARGS} -ge 2 ];then
-  TOC_DEPTH=$2
+SELF_CONTAINED=
+INPUT=
+
+while getopts "t:l:i:hs" arg; do
+  case "${arg}" in
+    "t")
+        TOC_DEPTH=${OPTARG} 
+        ;;
+    "l")
+        LANG=${OPTARG}
+        ;;
+    "i")
+        INPUT=${OPTARG:a}
+        ;;
+    "h")
+        print_usage_and_exit
+        ;;
+    "s")
+        echo "[INFO] create self contained html."
+        SELF_CONTAINED="--self-contained"
+        ;;
+    "?")  
+        echo "[WARNING] unkonw argument"
+        ;;
+    esac
+done
+
+if [ "${INPUT}" = "" ];then
+  echo "[ERROR] no input file."
+  print_usage_and_exit
 fi
 
-echo "Set toc_depth=${TOC_DEPTH}"
+if [ ${TOC_DEPTH} -lt 1 ];then
+  echo "[ERROR] toc-depth \033[0;32m${TOC_DEPTH}\033[0m, must >= 1."
+  print_usage_and_exit
+fi
+
+OUTPUT=${INPUT:a:r}.html
+
+echo "toc-depth=\033[0;32m${TOC_DEPTH}\033[0m"
+echo "lang=\033[0;32m${LANG}\033[0m"
+
+APP_DIR=${0:a:h}
 
 STYLE_HEADER=`mktemp`
-echo "<style>" > ${STYLE_HEADER}
-cat style.css >> ${STYLE_HEADER}
-echo "</style>" >> ${STYLE_HEADER}
+echo "<style>" > "${STYLE_HEADER}"
+cat "${APP_DIR}/style.css" >> "${STYLE_HEADER}"
+echo "</style>" >> "${STYLE_HEADER}"
 
-pandoc -f gfm -t html \
+pushd "${APP_DIR}"
+
+pandoc -f markdown -t html \
       --highlight-style=syntax.theme \
       --template=template.html \
-      -H ${STYLE_HEADER} \
+      -H "${STYLE_HEADER}" \
       --toc --toc-depth=${TOC_DEPTH} \
-      --standalone -o ${OUTPUT} ${INPUT}
+      --variable=lang:"${LANG}" \
+      --standalone ${SELF_CONTAINED} -o "${OUTPUT}" \
+      "${INPUT}"
 
-rm -f ${STYLE_HEADER}
-
-echo Output: "\033[0;32m ${OUTPUT} \033[0m"
-echo Done.
+rm -f "${STYLE_HEADER}"
